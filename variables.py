4500fb589857642e2c1f933f4ebebb08c18cbafd
vvlap <vvlap1976@gmail.com>
2024-11-20 13:41:17 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+homework_count=12
+hour_count=1.5
+course_name="Python"
+time_per_task=(hour_count/homework_count)
+print(course_name+", всего задач:"+str(homework_count)+", затрачено часов:"+str(hour_count)+", среднее время выполнения "+str(time_per_task)+" часа.")
